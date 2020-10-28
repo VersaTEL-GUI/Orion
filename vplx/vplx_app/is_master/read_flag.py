0e9594ae04a5ab926362a95db558fb3c3d306335
Ethan <ethan.lv@feixitek.com>
2020-10-28 14:22:01 +0800

    Modify read Master_ Flag file path

--- vplx/vplx_app/is_master/read_flag.py
+++ vplx/vplx_app/is_master/read_flag.py
@@ -3,11 +3,17 @@
 from vplx_app.is_master import is_master_blueprint
 from flask import jsonify
 
+
 def read_flag():
-    with open('../../master_flag')as f:
+    with open('master_flag')as f:
         r = f.read()
     return r
+
 
 @is_master_blueprint.route('/is_master')
 def is_master():
     return jsonify(read_flag())
+
+
+if __name__ == '__main__':
+    print('aaaa',read_flag())
